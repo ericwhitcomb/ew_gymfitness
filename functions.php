685a47c03df3a8a2b1fc7e5dfb6bdee9485e7ae5
Eric Whitcomb <eric.whitcomb@gmail.com>
2020-10-21 22:05:50 -0600

    Added style to page template and custom image sizes in functions.php

--- functions.php
+++ functions.php
@@ -28,4 +28,16 @@ function ew_gymfitness_scripts() {
 }
 add_action('wp_enqueue_scripts', 'ew_gymfitness_scripts');
 
+// Enable Feature images and other stuff
+function ew_gymfitness_setup() {
+    // Register new image sizes
+    add_image_size('square', 350, 350, true);
+    add_image_size('portrait', 350, 724, true);
+    add_image_size('box', 400, 375, true);
+    add_image_size('medium', 700, 400, true);
+    add_image_size('blog', 966, 644, true);
+    // Add featured image
+    add_theme_support( 'post-thumbnails');
+}
+add_action('after_setup_theme', 'ew_gymfitness_setup'); // When theme is activated and ready
 ?>
--- page.php
+++ page.php
@@ -1,8 +1,18 @@
 <?php get_header(); ?>
 
+<main class="container page section no-sidebars">
 <?php while(have_posts() ): the_post(); ?>
-    <h1><?php the_title(); ?></h1>
-    <p><?php the_content(); ?></p>
+    <h1 class="text-center text-primary"><?php the_title(); ?></h1>
+
+    <?php 
+        // check if an image exist
+        if (has_post_thumbnail()) {
+            the_post_thumbnail('blog', array('class' => 'featured-image'));
+        }
+    ?>
+    
+    <?php the_content(); ?>
 <?php endwhile; ?>
+</main>
 
 <?php get_footer(); ?>
